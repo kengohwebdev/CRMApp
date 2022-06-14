217cfcf71d61c0b95d8cbf39fa659ae8a0b547be
megahigh5 <kenaik_908@hotmail.com>
2022-06-14 18:40:45 -0500

    middleware backlog

--- CrmApi/Program.cs
+++ CrmApi/Program.cs
@@ -1,11 +1,17 @@
+using Antra.CrmAPI.Middleware;
 using Antra.CRMApp.Core.Contract.Repository;
 using Antra.CRMApp.Core.Contract.Service;
 using Antra.CRMApp.Infrastructure.Data;
 using Antra.CRMApp.Infrastructure.Repository;
 using Antra.CRMApp.Infrastructure.Service;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.SqlServer;
+using Serilog;
+using Serilog.AspNetCore;
 var builder = WebApplication.CreateBuilder(args);
+builder.Host.UseSerilog();
+
 
 // Add services to the container.
 
@@ -13,6 +19,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddLogging();
 builder.Services.AddSqlServer<CrmDbContext>(builder.Configuration.GetConnectionString("OnlineCRM"));
 builder.Services.AddScoped<IEmployeeRepositoryAsync, EmployeeRepositoryAsync>();
 builder.Services.AddScoped<IRegionRepositoryAsync, RegionRepositoryAsync>();
@@ -44,6 +51,26 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+
+//app.UseExceptionHandler(option =>
+//{
+//    option.Run(
+//        async context =>
+//        {
+//            var ex = context.Features.Get<IExceptionHandlerFeature>();
+//            if (ex != null)
+//            {
+//                await context.Response.WriteAsync(ex.Error.Message);
+//            }
+//        }
+//        );
+//}
+//    );
+
+
+app.UseSerilogRequestLogging();
+app.UseMiddleware<GlobalExceptionHandlerMiddleware>();
 
 app.UseHttpsRedirection();
 
